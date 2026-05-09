66fe2b81b73821e46e56355fe8e01491c02f18a5
minufy <seomeenu@proton.me>
2026-05-09 23:26:31 +0900

    offset

--- shadow.lua
+++ shadow.lua
@@ -2,7 +2,7 @@ Shadow = {}
 
 function Shadow:init(offset)
     self.shader = love.graphics.newShader("assets/shader/shadow.glsl")
-    self.offset = offset
+    self.offset = offset or {x = 4, y = 4}
     self.canvas = love.graphics.newCanvas(Res.w, Res.h)
 end
 
@@ -16,7 +16,7 @@ function Shadow:stop()
     love.graphics.setCanvas(Res.canvas)
     
     love.graphics.setShader(self.shader)
-    love.graphics.draw(self.canvas, Shader.offset, Shader.offset)
+    love.graphics.draw(self.canvas, self.offset.x, self.offset.y)
 
     love.graphics.setShader()
     love.graphics.draw(self.canvas)
