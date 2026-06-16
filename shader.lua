f7894a70f220682aba6d21e898f254505dbe271a
minufy <seomeenu@proton.me>
2026-06-16 16:41:05 +0900

    shadre

---
+++ shader.lua
@@ -0,0 +1,56 @@
+Shadow = {}
+
+function Shadow:init(offset)
+    self.shader = love.graphics.newShader("assets/shader/shadow.glsl")
+    self.offset = offset or {x = 4, y = 4}
+    self.canvas = love.graphics.newCanvas(Res.w, Res.h)
+end
+
+function Shadow:start()
+    love.graphics.setCanvas(self.canvas)
+    love.graphics.clear()
+end
+
+function Shadow:stop()
+    love.graphics.setBlendMode("alpha", "premultiplied")
+    love.graphics.setCanvas(Res.canvas)
+    
+    love.graphics.setShader(self.shader)
+    love.graphics.draw(self.canvas, self.offset.x, self.offset.y)
+
+    love.graphics.setShader()
+    love.graphics.draw(self.canvas)
+    
+    love.graphics.setBlendMode("alpha")
+end
+
+Outline = {}
+
+function Outline:init()
+    self.shader = love.graphics.newShader("assets/shader/outline.glsl")
+    self.canvas = love.graphics.newCanvas(Res.w, Res.h)
+end
+
+function Outline:start()
+    love.graphics.setCanvas(self.canvas)
+    love.graphics.clear()
+end
+
+function Outline:stop()
+    love.graphics.setBlendMode("alpha", "premultiplied")
+    love.graphics.setCanvas(Res.canvas)
+    
+    love.graphics.setShader(self.shader)
+    for x = -1, 1 do
+        for y = -1, 1 do
+            if not (x == 0 and y == 0) then
+                love.graphics.draw(self.canvas, x, y)
+            end
+        end
+    end
+
+    love.graphics.setShader()
+    love.graphics.draw(self.canvas)
+    
+    love.graphics.setBlendMode("alpha")
+end
